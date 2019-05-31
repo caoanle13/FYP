d0c7fdfbaaa86ea15beeb1702503a5739e8ba83b
Cao An <caoan.le15@imperial.ac.uk>
2019-05-31 07:38:18 +0100

    full transfer confirmation box

--- code/static/js/index.js
+++ code/static/js/index.js
@@ -1,6 +1,8 @@
 function validate(form) {
     var content = form.elements[0];
     var style = form.elements[1];
+    var select = form.elements[2];
+    var options = select.options;
   
     if (content.value === "") {
         alert("Please upload a content image.");
@@ -9,6 +11,9 @@ function validate(form) {
     else if (style.value === "") {
         alert("Please upload a style image as well.");
         style.focus();
+    }
+    else if (options.selectedIndex = 0) {
+        confirm("Perform style transfer without any guidance mask?");
     }
     else {
         return true;
