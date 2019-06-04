b4848fe16b76eeb5a74a5d2b9d7a0d0707a6cf5e
Cao An <caoan.le15@imperial.ac.uk>
2019-06-04 17:07:22 +0100

    added downloadable summary for output page

--- code/app.py
+++ code/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import os
 import shutil
 from segmentation.models import SemanticModel, ThresholdModel, ColourModel
 from segmentation.image_helpers import combine_masks
 from paths import *
-from clean import cleanup
+from structure import cleanup, add_summary_files, write_to_summary_file, produce_zip
 from stylisation.style_transfer_model import TransferModel
 
 app = Flask(__name__)
@@ -13,18 +13,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    cleanup()
     return render_template("index.html")
 
 @app.route("/masks", methods=["POST"])
 def masks():
 
-    content_image = request.files["content_image"]        
+    content_image = request.files["content_image"] 
     content_image.save(CONTENT_IMAGE_PATH)
 
     style_image = request.files["style_image"]
     style_image.save(STYLE_IMAGE_PATH)
 
     transfer_option = request.form["transfer-option"]
+
+    write_to_summary_file("CONTENT: " + content_image.filename)
+    write_to_summary_file("STYLE: " + style_image.filename) 
+    write_to_summary_file("OPTION: " + transfer_option)
+    add_summary_files([CONTENT_IMAGE_PATH, STYLE_IMAGE_PATH])
 
     if transfer_option == "full":
         return redirect(url_for('full_transfer'))
@@ -33,24 +39,50 @@ def masks():
         content_segmentor = SemanticModel(0)
         content_segmentor.segment(path=CONTENT_IMAGE_PATH)
         masks = [file for file in os.listdir(CONTENT_MASK_PATH) if file.startswith("mask_")]
+
+        masks_path = [os.path.join(CONTENT_MASK_PATH, mask) for mask in masks]
+        add_summary_files(masks_path)
+
         return render_template("semantic_masks.html", masks=masks)
 
     elif transfer_option == "threshold":
         n_threshold = int(request.form["n_threshold"])
+        write_to_summary_file("N THRESHOLD: " + str(n_threshold))
         
         content_segmentor = ThresholdModel(0, n_threshold)
         content_segmentor.segment(path=CONTENT_IMAGE_PATH)
         style_segmentor = ThresholdModel(1, n_threshold)
         style_segmentor.segment(path=STYLE_IMAGE_PATH)
+
+        masks_path = [
+            os.path.join(CONTENT_MASK_PATH, "threshold_mask.jpg"),
+            os.path.join(STYLE_MASK_PATH, "threshold_mask.jpg")
+            ]
+
+        add_summary_files(masks_path, ["content_threshold_mask.jpg", "style_threshold_mask.jpg"])
+        
+
         return render_template("threshold_masks.html", n=n_threshold)
 
+
     elif transfer_option == "colour":
+
         n_colours = int(request.form["n_colours"])
+        write_to_summary_file("N COLOUR: " + str(n_colours))
+
         base = int(request.form["base"])
+        write_to_summary_file("BASE: " + "style" if base else "content")
         
         colour_segmentor = ColourModel(base=base, n_colours=n_colours)
         colour_segmentor.segment(target=0)
         colour_segmentor.segment(target=1)
+
+        masks_path = [
+            os.path.join(CONTENT_MASK_PATH, "colour_mask.jpg"),
+            os.path.join(STYLE_MASK_PATH, "colour_mask.jpg")
+            ]
+
+        add_summary_files(masks_path, ["content_colour_mask.jpg", "style_colour_mask.jpg"])
         
         return render_template("colour_masks.html", n=n_colours)
 
@@ -67,6 +99,9 @@ def style_transfer():
         c_mask = os.path.join(CONTENT_MASK_PATH, "combined_mask.jpg")
         s_mask = None
         n_colors = 1
+
+        write_to_summary_file("SELECTED REGIONS: " + str(selected_masks))
+        add_summary_files([c_mask])
     
     elif transfer_option == "threshold":
         n_colors = int(request.form["n_colors"])
@@ -82,6 +117,8 @@ def style_transfer():
 
     model = TransferModel(n_colors, hard_width, c_mask, s_mask)
     model.apply_transfer()
+
+    add_summary_files([OUTPUT_IMAGE_PATH])
     
     return render_template("output.html", image="output.jpg")
 
@@ -93,11 +130,23 @@ def full_transfer():
     s_mask = None
     model = TransferModel(1, False, c_mask, s_mask)
     model.apply_transfer()
+    add_summary_files([OUTPUT_IMAGE_PATH], [None])
     return render_template("output.html", image="output.jpg")
+
+
+
+@app.route('/download_results')
+def request_zip():
+    data = produce_zip(SUMMARY_PATH)
+    return fl.send_file(
+        data,
+        mimetype='application/zip',
+        as_attachment=True,
+        attachment_filename='summary.zip'
+    )
 
 
 
     
 if __name__ == "__main__":
-    cleanup()
     app.run(host="localhost", port=8000, debug=False)
---
+++ code/structure.py
@@ -0,0 +1,75 @@
+import os
+from shutil import rmtree, copy
+from paths import *
+import zipfile
+import io
+import pathlib
+
+
+def cleanup():
+   """ Clean up the project directory tree when starting the app.
+   """
+    
+   if os.path.isdir(IMAGE_PATH):
+      rmtree(IMAGE_PATH)
+   os.mkdir(IMAGE_PATH)
+
+   if os.path.isdir(MASK_PATH):
+      rmtree(MASK_PATH)
+   os.mkdir(MASK_PATH)
+
+   if os.path.isdir(OUTPUT_PATH):
+      rmtree(OUTPUT_PATH)
+   os.mkdir(OUTPUT_PATH)
+
+   os.mkdir(CONTENT_MASK_PATH)
+   os.mkdir(STYLE_MASK_PATH)
+
+   if os.path.isdir(SUMMARY_PATH):
+      rmtree(SUMMARY_PATH)
+   os.mkdir(SUMMARY_PATH)
+   
+
+
+def add_summary_files(files, names=None):
+   """ Produces a summary folder with containing all the experiment details about the style transfer.
+   
+   Arguments:
+       - files {list}: List of file paths to copy to the summary directory.
+       - names {list}: List of names to replace the old ones.
+   """
+   if names is None:
+      names = [file.split('/')[-1] for file in files]
+
+   for file, name in zip(files, names):
+      copy(file, os.path.join(SUMMARY_PATH, name))
+
+
+def write_to_summary_file(text):
+
+   with open(SUMMARY_FILE_PATH, "a+") as f:
+      f.write(text +'\n')
+
+
+
+def produce_zip(folder):
+   """ Compresses a folder.
+   
+   Arguments:
+      - folder {str}: Path to the folder to be compressed.
+   
+   Returns:
+      - data: Compressed data.
+   """
+   base_path = pathlib.Path(folder)
+   data = io.BytesIO()
+   with zipfile.ZipFile(data, mode='w') as z:
+      for f_name in base_path.iterdir():
+         z.write(f_name)
+   data.seek(0)
+
+   return data
+
+
+
+
